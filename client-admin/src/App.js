b35ca51f251c9569d0376d3a14aa4ca613ad2e1a
GiangQuang <116422976+GiangQuang@users.noreply.github.com>
2023-07-25 23:16:36 +0700

    test 2

--- client-admin/src/App.js
+++ client-admin/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import MyProvider from './contexts/MyProvider';
 import Login from './components/LoginComponent';
 import Main from './components/MainComponent';
-axios.defaults.baseURL= "http://localhost:3000"
+axios.defaults.baseURL= "https://mern-stack-project-1z4i.onrender.com"
 
 class App extends Component {
   render() {
